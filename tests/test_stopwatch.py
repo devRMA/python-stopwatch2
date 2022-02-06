1cf508e24dc199a73d9344a03b84a4684517963c
Rafael <rafael271alves@gmail.com>
2022-02-06 20:06:01 -0300

    create the first test

---
+++ tests/test_stopwatch.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from time import sleep
+from stopwatch import Stopwatch
+
+
+class StopwatchTest(TestCase):
+
+    def test_stopwatch(self):
+        with Stopwatch() as sw:
+            sleep(0.1)
+        self.assertGreater(sw.elapsed, 0.0)
+        self.assertLess(sw.elapsed, 0.2)
+        self.assertTrue(str(sw).startswith('100') and str(sw).endswith('ms'))
