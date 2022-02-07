a0dd082781c8c6f96fceb011e6c6cb03f9a32507
Rafael <rafael271alves@gmail.com>
2022-02-07 12:09:56 -0300

    add more asserts

--- tests/test_stopwatch.py
+++ tests/test_stopwatch.py
@@ -1,14 +1,17 @@
-from unittest import TestCase
 from time import sleep
+from unittest import TestCase
+
 from stopwatch import Stopwatch
 
 
 # TODO : Make more tests
 class StopwatchTest(TestCase):
 
-    def test_stopwatch(self):
+    def test_stopwatch(self) -> None:
         with Stopwatch() as sw:
             sleep(0.1)
         self.assertGreater(sw.elapsed, 0.0)
         self.assertLess(sw.elapsed, 0.2)
+        self.assertEqual(len(sw.laps), 1)
+        self.assertFalse(sw.running)
         self.assertTrue(str(sw).startswith('100') and str(sw).endswith('ms'))
