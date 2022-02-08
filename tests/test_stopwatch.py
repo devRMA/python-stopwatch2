b5ee9720b7c39085befb08c767b9acd467822057
Rafael <rafael271alves@gmail.com>
2022-02-08 16:35:48 -0300

    new tests

--- tests/test_stopwatch.py
+++ tests/test_stopwatch.py
@@ -56,6 +56,19 @@ class StopwatchTest(TestCase):
         print_mock.assert_called_once()
         print_mock.assert_called_with(sw._format())
 
+    @patch('builtins.print')
+    def test_stopwatch_print_report_with_name(
+        self, print_mock: MagicMock
+    ) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch(name='lorem', print_report=True) as sw:
+                self.time_mock.increment(0.1)
+        print_mock.assert_called_once()
+        print_mock.assert_called_with(sw._format())
+        self.assertTrue(sw._format().endswith('lorem'))
+        sw._print_report = False
+        self.assertEqual(sw._format(), '')
+
     def test_stopwatch_reset(self) -> None:
         with patch('time.perf_counter', self.time_mock.perf_counter):
             with Stopwatch() as sw:
@@ -87,3 +100,26 @@ class StopwatchTest(TestCase):
         self.assertEqual(sw.elapsed, 10)
         self.assertEqual(len(sw.laps), 5)
         self.assertEqual(sw.laps, [i for i in range(5)])
+
+    def test_stopwatch_report_with_on_lap(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch('sw1') as sw1:
+                self.time_mock.increment(1)
+            with Stopwatch() as sw2:
+                self.time_mock.increment(1)
+            with Stopwatch(precision=4) as sw3:
+                self.time_mock.increment(1)
+        self.assertEqual(sw1.report(), '[Stopwatch#sw1] total=1.00s')
+        self.assertEqual(sw2.report(), '[Stopwatch] total=1.00s')
+        self.assertEqual(sw3.report(), '[Stopwatch] total=1.0000s')
+
+    def test_stopwatch_report_with_laps(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch() as sw:
+                for i in range(5):
+                    with sw.lap():
+                        self.time_mock.increment(i)
+        self.assertEqual(
+            sw.report(), '[Stopwatch] total=10.00s, mean=2.0000s, '
+            'min=0.0000s, median=2.0000s, max=4.0000s, dev=1.4142s'
+        )
