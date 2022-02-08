e81bb028ea41ac77f1bd10453a12dfcdb9594c8e
Rafael <rafael271alves@gmail.com>
2022-02-07 22:24:46 -0300

    new asserts

--- tests/test_stopwatch.py
+++ tests/test_stopwatch.py
@@ -8,10 +8,17 @@ from stopwatch import Stopwatch
 class StopwatchTest(TestCase):
 
     def test_stopwatch(self) -> None:
-        with Stopwatch() as sw:
+        with Stopwatch('lorem') as sw:
             sleep(0.1)
         self.assertGreater(sw.elapsed, 0.0)
         self.assertLess(sw.elapsed, 0.2)
         self.assertEqual(len(sw.laps), 1)
         self.assertFalse(sw.running)
-        self.assertTrue(str(sw).startswith('100') and str(sw).endswith('ms'))
+        self.assertTrue(
+            all([str(sw).startswith('100'),
+                 str(sw).endswith('ms')]))
+        self.assertTrue(
+            all([
+                repr(sw).startswith('<Stopwatch name=lorem elapsed='),
+                repr(sw).endswith('>')
+            ]))
