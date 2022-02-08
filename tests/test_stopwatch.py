fdf8b24012259549e96c57effca5f63402034251
Rafael <rafael271alves@gmail.com>
2022-02-08 16:12:46 -0300

    added new tests

--- tests/test_stopwatch.py
+++ tests/test_stopwatch.py
@@ -1,27 +1,89 @@
-from time import sleep
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
 from stopwatch import Stopwatch
 
+from .mocks.time import MockTime
 
-# TODO : Make more tests
+
 class StopwatchTest(TestCase):
-    def test_stopwatch(self) -> None:
-        with Stopwatch('lorem') as sw:
-            sleep(0.1)
-        self.assertGreater(sw.elapsed, 0.0)
-        self.assertLess(sw.elapsed, 0.2)
+    def setUp(self) -> None:
+        self.time_mock = MockTime()
+        return super().setUp()
+
+    def test_stopwatch_basic_usage(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            sw = Stopwatch()
+            self.time_mock.increment(0.1)  # simulate time passing
+            sw.stop()
+        self.assertEqual(sw.elapsed, 0.1)
         self.assertEqual(len(sw.laps), 1)
         self.assertFalse(sw.running)
-        self.assertTrue(
-            all([str(sw).startswith('100'),
-                 str(sw).endswith('ms')])
-        )
-        self.assertTrue(
-            all(
-                [
-                    repr(sw).startswith('<Stopwatch name=lorem elapsed='),
-                    repr(sw).endswith('>')
-                ]
-            )
-        )
+        self.assertEqual(str(sw), '100.00ms')
+        self.assertEqual(repr(sw), '<Stopwatch name=None elapsed=0.1>')
+        self.assertIsNone(sw.name)
+
+    def test_stopwatch_with_statement(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch() as sw:
+                self.time_mock.increment(0.1)
+                self.assertTrue(sw.running)
+        self.assertEqual(sw.elapsed, 0.1)
+        self.assertEqual(len(sw.laps), 1)
+        self.assertFalse(sw.running)
+        self.assertEqual(str(sw), '100.00ms')
+        self.assertEqual(repr(sw), '<Stopwatch name=None elapsed=0.1>')
+        self.assertIsNone(sw.name)
+
+    def test_stopwatch_with_name(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch(name='lorem') as sw:
+                self.time_mock.increment(0.1)
+        self.assertIsNotNone(sw.name)
+        self.assertEqual(sw.name, 'lorem')
+
+    def test_stopwatch_precision(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch(precision=5) as sw:
+                self.time_mock.increment(0.123456789)
+        self.assertEqual(str(sw), '123.45679ms')
+
+    @patch('builtins.print')
+    def test_stopwatch_print_report(self, print_mock: MagicMock) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch(print_report=True) as sw:
+                self.time_mock.increment(0.1)
+        print_mock.assert_called_once()
+        print_mock.assert_called_with(sw._format())
+
+    def test_stopwatch_reset(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch() as sw:
+                self.time_mock.increment(0.1)
+        self.assertEqual(sw.elapsed, 0.1)
+        self.assertEqual(len(sw.laps), 1)
+        sw.reset()
+        self.assertEqual(sw.elapsed, 0.0)
+        self.assertEqual(len(sw.laps), 0)
+
+    def test_stopwatch_restart(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch() as sw:
+                self.time_mock.increment(0.1)
+            self.assertEqual(sw.elapsed, 0.1)
+            self.assertEqual(len(sw.laps), 1)
+            sw.restart()
+            self.time_mock.increment(0.1)
+            sw.stop()
+            self.assertEqual(sw.elapsed, 0.1)
+            self.assertEqual(len(sw.laps), 1)
+
+    def test_stopwatch_lap(self) -> None:
+        with patch('time.perf_counter', self.time_mock.perf_counter):
+            with Stopwatch() as sw:
+                for i in range(5):
+                    with sw.lap():
+                        self.time_mock.increment(i)
+        self.assertEqual(sw.elapsed, 10)
+        self.assertEqual(len(sw.laps), 5)
+        self.assertEqual(sw.laps, [i for i in range(5)])
