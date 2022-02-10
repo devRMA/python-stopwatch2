565e87db416e020e36fb46c2666f563512beb8ff
Rafael <rafael271alves@gmail.com>
2022-02-10 20:20:16 -0300

    test refactored with pytest-mock and pytest-describe

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,30 @@
-from unittest import TestCase
-from unittest.mock import patch
+from pytest_mock import MockerFixture
 
 from stopwatch import utils
 
 from .mocks.inspect import InspectMock
 
 
-class UtilsTest(TestCase):
-    def test_format_elapsed_time(self) -> None:
-        self.assertEqual(utils.format_elapsed_time(1, 2), '1.00s')
-        self.assertEqual(utils.format_elapsed_time(1, 4), '1.0000s')
-        self.assertEqual(utils.format_elapsed_time(1, 0), '1s')
-        self.assertEqual(utils.format_elapsed_time(0.1, 0), '100ms')
-        self.assertEqual(utils.format_elapsed_time(0.001, 0), '1ms')
-        self.assertEqual(utils.format_elapsed_time(0.0001, 0), '100μs')
-        self.assertEqual(utils.format_elapsed_time(0.000001, 0), '1μs')
+def describe_format_elapsed_time() -> None:
+    def without_precision() -> None:
+        assert utils.format_elapsed_time(0) == '0.00μs'
+        assert utils.format_elapsed_time(1) == '1.00s'
+        assert utils.format_elapsed_time(0.01) == '10.00ms'
+        assert utils.format_elapsed_time(0.001) == '1.00ms'
+        assert utils.format_elapsed_time(0.0001) == '100.00μs'
 
-    def test_inspect_caller(self) -> None:
-        with patch('inspect.stack', InspectMock.stack):
-            caller = utils.inspect_caller()
-        self.assertEqual(caller.module, '<unknown>')
-        self.assertEqual(caller.function, 'function')
-        self.assertEqual(caller.line_number, 2)
+    def with_precision() -> None:
+        assert utils.format_elapsed_time(0, 1) == '0.0μs'
+        assert utils.format_elapsed_time(1, 3) == '1.000s'
+        assert utils.format_elapsed_time(0.01, 5) == '10.00000ms'
+        assert utils.format_elapsed_time(0.001, 0) == '1ms'
+        assert utils.format_elapsed_time(0.0001, 2) == '100.00μs'
+
+
+def describe_inspect_caller() -> None:
+    def returns_caller_information(mocker: MockerFixture) -> None:
+        mocker.patch('inspect.stack', InspectMock.stack)
+        caller = utils.inspect_caller()
+        assert caller.module == '<unknown>'
+        assert caller.function == 'function'
+        assert caller.line_number == 2
