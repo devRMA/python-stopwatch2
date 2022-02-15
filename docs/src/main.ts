c0b1e4503ec563b29cfab11079a63e6a2b408363
Rafael <rafael271alves@gmail.com>
2022-02-15 11:16:51 -0300

    added import fonts

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
+
+// General Font
+import 'vfonts/Lato.css'
+// Monospace Font
+import 'vfonts/FiraCode.css'
+
 import App from "./App.vue";
 
 const app = createApp(App);
