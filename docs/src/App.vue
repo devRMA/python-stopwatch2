0482aad41dafc5d43d7ba31057522bd7b33fd278
Rafael <rafael271alves@gmail.com>
2022-02-15 13:59:15 -0300

    added naive ui globally

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,14 +1,29 @@
-<script setup lang="ts">
-import { NButton } from 'naive-ui'
-import { ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from "vue";
+import { darkTheme, lightTheme, useOsTheme } from "naive-ui";
 
-const counter = ref(0)
+let themeToUse = darkTheme;
+const themeInLocalStorage = localStorage.getItem("theme");
+if (themeInLocalStorage == null) {
+    themeToUse = useOsTheme().value === "dark" ? darkTheme : lightTheme;
+} else {
+    themeToUse = themeInLocalStorage === "dark" ? darkTheme : lightTheme;
+}
+
+const theme = ref(themeToUse);
 </script>
 
 <template>
-    Hello World
-    <n-button @click="counter++">{{ counter }} +</n-button>
+    <n-config-provider :theme="theme">
+        <n-card>
+            <n-space>
+                <n-button @click="theme = darkTheme">Dark</n-button>
+                <n-button @click="theme = lightTheme">Light</n-button>
+            </n-space>
+        </n-card>
+    </n-config-provider>
 </template>
 
 <style lang="sass">
+
 </style>
--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
+import naive from "naive-ui";
 
 // General Font
-import 'vfonts/Lato.css'
+import "vfonts/Lato.css";
 // Monospace Font
-import 'vfonts/FiraCode.css'
+import "vfonts/FiraCode.css";
 
 import App from "./App.vue";
 
 const app = createApp(App);
+
+app.use(naive);
 
 app.mount("#app");
