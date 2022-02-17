1a8089284c4c2e8bbe27364de10e3217b4f784bb
Rafael <rafael271alves@gmail.com>
2022-02-17 13:29:49 -0300

    python files removed from branch

--- stopwatch/__init__.py
+++
@@ -1,6 +0,0 @@
-from .profile import profile
-from .statistics import Statistics
-from .stopwatch import Stopwatch
-from .utils import format_elapsed_time
-
-__all__ = ['Statistics', 'Stopwatch', 'format_elapsed_time', 'profile']
--- tests/mocks/atexit.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Callable
-
-
-class AtexitMock:
-    @staticmethod
-    def register(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
-        return func(*args, **kwargs)
