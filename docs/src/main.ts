1dc308250573c89bdbc4df832815df58c60c378b
Rafael <rafael271alves@gmail.com>
2022-02-16 22:24:10 -0300

    initial theme setup moved to main.ts and SASS import added

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,8 +1,15 @@
-import { createApp } from "vue";
 import "@varlet/touch-emulator";
+import { createApp } from "vue";
 
+import { changeTheme } from "./utils/theme";
 import App from "./App.vue";
+import './styles/main.sass';
 
+// Setting the theme
+if (!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "dark");
+}
+changeTheme(localStorage.getItem("theme"));
 
 const app = createApp(App);
 
