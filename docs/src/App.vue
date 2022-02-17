b667a8a952f417a5fc783abf3e3f69ec7b568fc1
Rafael <rafael271alves@gmail.com>
2022-02-16 22:25:14 -0300

    using the component that switch the theme

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,23 +1,7 @@
 <script lang="ts" setup>
-import { changeTheme } from "./utils/theme";
-
-if (!localStorage.getItem("theme")) {
-    localStorage.setItem("theme", "dark");
-}
-
-changeTheme(localStorage.getItem("theme"));
+import SwitchTheme from "./components/SwitchTheme.vue";
 </script>
 
 <template>
-    <var-button type="success" @click="changeTheme('dark')"> Dark </var-button>
-    <var-button type="success" @click="changeTheme('light')">
-        Light
-    </var-button>
+    <SwitchTheme />
 </template>
-
-<style lang="sass">
-body
-    transition: background-color .25s
-    color: var(--color-text)
-    background-color: var(--color-body)
-</style>
