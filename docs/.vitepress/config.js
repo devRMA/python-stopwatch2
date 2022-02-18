8bbe9eabb24d74ed1754256fb3d42df65f606934
Rafael <rafael271alves@gmail.com>
2022-02-18 10:34:27 -0300

    updated settings and css

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,17 +1,163 @@
-import { defineConfig } from "vitepress";
+// @ts-check
 
-export default defineConfig({
+/*
+ * Config based on pinia and vite
+ * https://github.com/vuejs/pinia/blob/13d12710da139db8a92ae5509b89d00097f22a8f/packages/docs/.vitepress/config.js
+ * https://github.com/vitejs/vite/blob/a7743039f263f41e1c3971e324f893a5ef5e5508/docs/.vitepress/config.js
+ */
+
+const META_URL = "https://stopwatch2.vercel.app/";
+const META_TITLE = "Python Stopwatch 2 ⏱";
+const META_SITE_NAME = "Python Stopwatch 2";
+const META_DESCRIPTION = "A simple library to measure code performance.";
+const META_IMAGE = "https://stopwatch2.vercel.app/logo_shadow.png";
+
+const isProduction = process.env.NODE_ENV;
+
+/**
+ * @type {import('vitepress').UserConfig['head']}
+ */
+const productionHead = [
+    [
+        "script",
+        {
+            src: "https://unpkg.com/thesemetrics@latest",
+            async: "",
+            type: "text/javascript",
+        },
+    ],
+];
+
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+module.exports = {
     lang: "en-US",
     title: "Python Stopwatch 2",
     description: "A simple library to measure code performance.",
     lastUpdated: true,
+    head: [
+        ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
+        [
+            "meta",
+            {
+                property: "theme-color",
+                content: "#ffd859",
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:locale",
+                content: "en-US",
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:type",
+                content: "website",
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:url",
+                content: META_URL,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:site_name",
+                content: META_SITE_NAME,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:title",
+                content: META_TITLE,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:description",
+                content: META_DESCRIPTION,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "og:image",
+                content: META_IMAGE,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:card",
+                content: "summary_large_image",
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:url",
+                content: META_URL,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:title",
+                content: META_TITLE,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:description",
+                content: META_DESCRIPTION,
+            },
+        ],
+        [
+            "meta",
+            {
+                property: "twitter:image",
+                content: META_IMAGE,
+            },
+        ],
+
+        ...(isProduction ? productionHead : []),
+    ],
     themeConfig: {
         repo: "devRMA/python-stopwatch2",
+        logo: "/logo.png",
         docsDir: "docs",
         docsBranch: "docs",
         editLinks: true,
-        editLinkText: "Edit this page on GitHub",
+        editLinkText: "Suggest changes to this page",
         lastUpdated: "Last Updated",
+
+        nav: [
+            { text: "Guide", link: "/guide/getting-started" },
+            { text: "API", link: "/guide/api" },
+            {
+                text: "Links",
+                items: [
+                    {
+                        text: "Issues",
+                        link: "https://github.com/devRMA/python-stopwatch2/issues",
+                    },
+                    {
+                        text: "Changelog",
+                        link: "https://github.com/devRMA/python-stopwatch2/blob/main/CHANGELOG.md",
+                    },
+                ],
+            },
+        ],
 
         sidebar: {
             "/": [
@@ -48,4 +194,4 @@ export default defineConfig({
             ],
         },
     },
-});
+};
