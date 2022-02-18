17ad6bca8806ae9e3e7784f3e56dbd6a1cb4ef16
Rafael <rafael271alves@gmail.com>
2022-02-18 11:23:11 -0300

    create file to skip vercel build

---
+++ .vercel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$VERCEL_ENV" == "production" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
